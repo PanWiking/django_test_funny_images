d9671ebd7272bba425fe29b180ee09248c8a04ab
Wiking <miodekip@gmail.com>
2021-03-21 17:12:39 +0100

    simple view for pages

--- funny_webs/funnys/models.py
+++ funny_webs/funnys/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Funny(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # web = models.FileField()
--- funny_webs/funnys/views.py
+++ funny_webs/funnys/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from funnys.models import Funny
+
+
+def post_list(request):
+    funnys = Funny.objects.all()
+    return render(request, 'funnys/list.html', {'funnys': funnys})
